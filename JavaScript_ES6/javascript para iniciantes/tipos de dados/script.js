f44802a724a32d6a1c00d409988d06b826982c7c
Thaina Mai <mnagahama21@gmail.com>
2021-04-27 18:08:43 -0300

    dia 01 js6

---
+++ JavaScript_ES6/javascript para iniciantes/tipos de dados/script.js
@@ -0,0 +1,21 @@
+//Declare uma variável contendo uma string
+var nome = 'mai'
+
+//Declare uma variável contendo um número dentro de uma string
+var ano= '2021' 
+
+//Declare uma variável com a sua idade
+var idade = 24
+
+//Declare duas variáveis uma com o seu nome
+//e outra com seu sobrenome e some os mesmos
+var sobrenome = 'nagahama'
+    nomeCompleto = `${nome} ${sobrenome}`
+console.log(nomeCompleto)    
+
+//Coloque a seguinte frase em uma variável: It's time
+var frase = 'It\'s time'
+console.log(frase)
+
+//Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome)
---
+++ JavaScript_ES6/javascript para iniciantes/variaveis/script.js
@@ -0,0 +1,20 @@
+//Declarar uma variável com o seu nome
+var nome = 'mai';
+
+//Declarar uma variável com a sua idade
+var idade = '24';
+
+//Declarar uma variável com a sua comida 
+//favorita e não atribuir valor
+var comidaFavorita;
+
+//Atribuir o valor a sua comida favorita
+comidaFavorita = 'lasanha';
+
+//Declarar 5 variáveis diferentes sem valores
+var time,
+    país,
+    endereco,
+    rua;
+    
+console.log(nome, idade, comidaFavorita);
