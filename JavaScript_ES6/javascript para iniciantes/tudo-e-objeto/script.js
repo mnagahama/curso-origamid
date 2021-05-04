6fe02cc413be831bf30b868313a8c0ce29df4037
Thaina Mai <mnagahama21@gmail.com>
2021-05-04 18:46:06 -0300

    modulo finalizado 🎉️

---
+++ JavaScript_ES6/javascript para iniciantes/tudo-e-objeto/script.js
@@ -0,0 +1,16 @@
+// nomeie 3 propriedades ou métodos de strings
+var  nome = 'Mai'
+fixed
+length
+slice
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+var btn = document.querySelector('.btn')
+//addEventListener
+//id
+//appendChild
+//innerHhtml
+//outerHtml
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
